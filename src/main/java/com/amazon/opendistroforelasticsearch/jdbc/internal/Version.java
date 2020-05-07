fcef2a095081f86050dc1d03cd96f1ecf236082e
Abbas Hussain <abbashus@amazon.com>
2020-05-07 14:01:02 -0700

    OpenDistro Release 1.7.0.0

--- src/main/java/com/amazon/opendistroforelasticsearch/jdbc/internal/Version.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/jdbc/internal/Version.java
@@ -19,7 +19,7 @@ package com.amazon.opendistroforelasticsearch.jdbc.internal;
 public enum Version {
 
     // keep this in sync with the gradle version
-    Current(1, 6, 1, 0);
+    Current(1, 7, 0, 0);
 
     private int major;
     private int minor;
